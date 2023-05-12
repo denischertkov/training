66cdd4f099912af1152009274bb0d2970d23986f
Denis Chertkov <denisc@wix.com>
2023-05-12 22:04:05 +0300

    Optimization of the pod filtering algorithm

--- kubeget/kubeget.py
+++ kubeget/kubeget.py
@@ -1,5 +1,5 @@
 
-# KUBEGET v.0.9. This program list the pods on the K8s cluster. 
+# KUBEGET v.0.9.5. This program list the pods on the K8s cluster. 
 # Commandline options:
 # --node [nodename]       - show only the Pods from a given node
 # --namespace [NAMESPACE] - show the only Pods from the given namespace
@@ -42,49 +42,54 @@ def showPods(namespace, nodename):
         stdout_string = subprocess.check_output(
             ['kubectl', 'get', 'pod', '--all-namespaces', '-o', 'json'], universal_newlines=True)
     except:
-        print('ERROR: Cant get the pods from the cluster')
+        print('error: Can\'t get the pods from the cluster')
+        exit(1)
     
     stdout_struct = json.loads(stdout_string)
 
     # Fill the pods structure
     pods = []
     for pod in stdout_struct['items']:
-        pods.append({'name': pod['metadata']['name'],                               # Pod name
-                     'namespace': pod['metadata']['namespace'],                     # Namespace
-                     'nodename': pod['spec']['nodeName'],                           # NodeName
-                     'labels': len(pod['metadata']['labels']),                      # Amounts of labels
-                     })
+        # Do we need to add the current pod to the list?
+        list_pod = False
+        if namespace == None:                                                       # if namespace not defined
+            if nodename == None:                                                    # if namespace AND node not defined
+                list_pod = True
+            # if namespace defined AND node not defined
+            else:
+                if nodename == pod['spec']['nodeName']:
+                    list_pod = True
+        else:                                                                       # if namespace is defined
+            # if namespace is defined AND node not defined
+            if nodename == None:
+                if namespace == pod['metadata']['namespace']:
+                    list_pod = True
+            # namespace is defined AND nodename also defined
+            else:
+                if (namespace == pod['metadata']['namespace'] and nodename == pod['spec']['nodeName']):
+                    list_pod = True
+        if list_pod:
+            pods.append({'name': pod['metadata']['name'],                           # Pod name
+                        'namespace': pod['metadata']['namespace'],                  # Namespace
+                        'nodename': pod['spec']['nodeName'],                        # NodeName
+                        'labels': len(pod['metadata']['labels']),                   # Amounts of labels
+                        })
 
     # Show the pods
     print('POD NAME                                         Number of Labels   Node Name                                        NAMESPACE')
     print('=========                                        ================   ==========                                       ===========')
     
-    # Do we need to list the current pod?
+    # List the pods
     for pod in pods:
-        list_pod = False
-        if namespace == None:                                                       # if namespace not defined
-            if nodename == None:                                                    # if namespace AND node not defined
-                list_pod = True
-            else:                                                                   # if namespace defined AND node not defined
-                if nodename == pod['nodename']:
-                    list_pod = True
-        else:                                                                       # if namespace is defined
-            if nodename == None:                                                    # if namespace is defined AND node not defined
-                if namespace == pod['namespace']:
-                    list_pod = True
-            else:                                                                   # namespace is defined AND nodename also defined
-                if (namespace == pod['namespace'] and nodename == pod['nodename']):
-                    list_pod = True
-        if list_pod:
-            out_line = '{0:49}{1:<19d}{2:49}{3:27}'.format(
-                pod['name'][:48], pod['labels'], pod['nodename'][:48], pod['namespace'][:26])
-            print(out_line)
+        out_line = '{0:49}{1:<19d}{2:49}{3:27}'.format(
+            pod['name'][:48], pod['labels'], pod['nodename'][:48], pod['namespace'][:26])
+        print(out_line)
     # print('=========                                        ================   ==========                                ===========')
     # print('POD NAME                                         Number of Labels   Node Name                                 NAMESPACE')
 
 if __name__ == '__main__':
 
-    print("KUBEGET v.0.9. This program list the pods on the K8s cluster. Run with -h option for help.")
+    print("KUBEGET v.0.9.5. This program list the pods on the K8s cluster. Run with -h option for help.")
     print("Denis Chertkov, denis@chertkov.info, 12/05/2023")
 
     if not kubectlIsInstalled():
